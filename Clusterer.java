9cf3cce1023544e9eec92cb27a5e84dce931e80e
Landon Davidson <accounts@landond.tech>
2025-03-09 18:46:22 -0700

    Add: rough draft constructor

--- Clusterer.java
+++ Clusterer.java
@@ -7,7 +7,26 @@ public class Clusterer {
     private double cost; // the distance between the closest pair of clusters
 
     public Clusterer(double[][] distances, int k){
-        // TODO
+        // Initialize minimum spanning stree adjancency list with n items, will be populated by
+        mstAdjList = new ArrayList<>(distances.length);
+
+        // Initialize the clusters list with k items
+        clusters = new ArrayList<>(k);
+        for (int i = 0; i < k; i++) {
+            clusters.add(new LinkedList<Integer>());
+        }
+
+        // Populate adjacency list
+        adjList = new ArrayList<>(distances.length);
+        for (int i = 0; i < distances.length; i++) {
+            // Create linked list for current node
+            adjList.add(new LinkedList<WeightedEdge<Integer, Double>>());
+            List<WeightedEdge<Integer, Double>> cur_node = adjList.get(i);
+            for (int j = 0; j < distances[i].length; j++) {
+                // Add each edge from current node i to the linked list
+                cur_node.add(new WeightedEdge<Integer,Double>(i, j, distances[i][j]));
+            }
+        }      
     }
 
     // implement Prim's algorithm to find a MST of the graph.
