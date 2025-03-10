adc01f6c2c6572818dbc4578bfba1d9f9c7888e6
Landon Davidson <accounts@landond.tech>
2025-03-09 19:32:05 -0700

    Add: rough draft prims implementation

--- Clusterer.java
+++ Clusterer.java
@@ -35,7 +35,42 @@ public class Clusterer {
     // implement Prim's algorithm to find a MST of the graph.
     // in my implementation I used the mstAdjList field to store this.
     private void prims(int start){
-        //TODO
+        // Create known array and size value
+        // double[] costs = new double[adjList.size()];
+        // Arrays.fill(costs, Double.MAX_VALUE);
+        boolean[] known = new boolean[adjList.size()];
+        Arrays.fill(known, false);
+        int size = 1;
+
+        // Initialize priority queue with start node's edges
+        known[start] = true;
+        //costs[start] = 0;
+        PriorityQueue<WeightedEdge<Integer, Double>> queue = new PriorityQueue<>();
+        for (WeightedEdge<Integer, Double> edge : adjList.get(start)) {
+            queue.add(edge);
+        }
+
+        // Loop through priority queue until every node is added
+        while(size != adjList.size()) {
+            // Get cheapest edge
+            WeightedEdge<Integer, Double> cur_edge = queue.remove();
+            // If edge destination is known skip it
+            if (known[cur_edge.destination]) continue;
+            // Otherwise mark it known and add to mst
+            known[cur_edge.destination] = true;
+            mstAdjList.get(cur_edge.source).add(cur_edge);
+            // Also add mirrored edge to destination index
+            WeightedEdge<Integer, Double> mirror_edge = new WeightedEdge<Integer,Double>(cur_edge.destination, cur_edge.source, cur_edge.weight);
+            mstAdjList.get(mirror_edge.source).add(mirror_edge);
+            // Increment size so we can break the loop when done
+            size++;
+            // Add each of the destination's edges to the priority queue if they aren't known
+            for (WeightedEdge<Integer, Double> edge : adjList.get(cur_edge.destination)) {
+                if (known[edge.destination]) continue;
+                queue.add(edge);
+            }
+        }
+        size = 0;
     }
 
 
